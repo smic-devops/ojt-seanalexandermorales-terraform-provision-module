ec764c95f06243698daa89f626abc4da02088512
ojt-smic-seanalexandermorales <i.sean.morales@sminvestments.com>
2026-02-11 16:43:04 +0800

    Updated default values

--- variables.tf
+++ variables.tf
@@ -1,25 +1,36 @@
 variable "prefix" {
   type        = string
   description = "A naming prefix for resources"
+  default = "itss-ojt-morales"
 }
 
 variable "vpc_id" {
   type        = string
   description = "The ID of the existing VPC"
+  default = "vpc-05596861f4ecffdeb"
 }
 
 variable "public_subnet_ids" {
   type        = list(string)
   description = "The IDs of the public subnets for the ALB"
+  default = [
+    "subnet-02f95b7899e8bed30", # itss-ops az1 public
+    "subnet-09c8dbaa942884f5d"  # itss-ops az2 public
+  ]
 }
 
 variable "private_subnet_ids" {
   type        = list(string)
   description = "The IDs of the private subnets for the EC2"
+  default = [
+    "subnet-0ee426ba08e9643d9", # itss-ops az1 private
+    "subnet-095cdc7b816291369"  # itss-ops az2 private
+  ]
 }
 
 variable "ami_id" {
   type        = string
+  default = "ami-0249e9b9816d90e03"
 }
 
 variable "instance_type" {
