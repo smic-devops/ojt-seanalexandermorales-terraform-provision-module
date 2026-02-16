9710de2510ea9f231a4195a3614270b65812eea9
ojt-smic-seanalexandermorales <i.sean.morales@sminvestments.com>
2026-02-16 11:55:51 +0800

    Updated main.tf for security module

--- security/main.tf
+++ security/main.tf
@@ -2,41 +2,28 @@ resource "aws_security_group" "alb_sg" {
   name   = "${var.prefix}-alb-sg-module"
   vpc_id = var.vpc_id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Environment    = "Sandbox"
     Resource_Types = "Instances Volumes Network_Interfaces"
   }
 }
 
-resource "aws_security_group" "ec2_sg" {
-  name   = "${var.prefix}-ec2-sg-module"
-  vpc_id = var.vpc_id
+resource "aws_security_group_rule" "alb_ingress_from_http" {
+  type = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Environment    = "Sandbox"
-    Resource_Types = "Instances Volumes Network_Interfaces"
-  }
+resource "aws_security_group_rule" "alb_ingres_from_https" {
+  type = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
 }
 
 resource "aws_security_group_rule" "alb_egress_to_ec2" {
@@ -48,6 +35,16 @@ resource "aws_security_group_rule" "alb_egress_to_ec2" {
   source_security_group_id = aws_security_group.ec2_sg.id
 }
 
+resource "aws_security_group" "ec2_sg" {
+  name   = "${var.prefix}-ec2-sg-module"
+  vpc_id = var.vpc_id
+
+  tags = {
+    Environment    = "Sandbox"
+    Resource_Types = "Instances Volumes Network_Interfaces"
+  }
+}
+
 resource "aws_security_group_rule" "ec2_ingress_from_alb" {
   type                     = "ingress"
   from_port                = 80
@@ -57,26 +54,39 @@ resource "aws_security_group_rule" "ec2_ingress_from_alb" {
   source_security_group_id = aws_security_group.alb_sg.id
 }
 
+resource "aws_security_group_rule" "ec2_egress_to_all" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.ec2_sg.id
+}
+
 resource "aws_security_group" "rds_sg" {
   name   = "${var.prefix}-rds-sg-module"
   vpc_id = var.vpc_id
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Environment    = "Sandbox"
     Resource_Types = "Instances Volumes Network_Interfaces"
   }
+}
+
+resource "aws_security_group_rule" "rds_ingress_from_ec2" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "rds_egress_to_all" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.rds_sg.id
 }
