7084b609956219f18d3d22a0265d7ffef0d574b7
ojt-smic-seanalexandermorales <i.sean.morales@sminvestments.com>
2026-03-18 14:03:27 +0800

    Removed RDS via Terraform

--- output.tf
+++ output.tf
@@ -3,6 +3,6 @@ output "root_instance_private_dns" {
   value       = module.itss-ojt-ec2.ec2_instance_private_dns
 }
 
-output "rds_endpoint" {
-  value = module.itss-ojt-rds.db_instance_endpoint
-}
+# output "rds_endpoint" {
+#   value = module.itss-ojt-rds.db_instance_endpoint
+# }
