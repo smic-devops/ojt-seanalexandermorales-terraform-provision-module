7bb842e1e0edbd3115506c6e786e26918492fc15
ojt-smic-seanalexandermorales <i.sean.morales@sminvestments.com>
2026-03-18 14:01:05 +0800

    Removed RDS via Terraform

--- rds_main.tf
+++ rds_main.tf
@@ -1,8 +1,8 @@
-module "itss-ojt-rds" {
-    source = "./rds"
-    prefix = var.prefix
-    username = var.username
-    password = var.password
-    rds_sg_id = module.itss-ojt-security.rds_sg_id
-    private_subnet_ids = var.private_subnet_ids
-}
+# module "itss-ojt-rds" {
+#     source = "./rds"
+#     prefix = var.prefix
+#     username = var.username
+#     password = var.password
+#     rds_sg_id = module.itss-ojt-security.rds_sg_id
+#     private_subnet_ids = var.private_subnet_ids
+# }
